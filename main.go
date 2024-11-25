ad88f7d3105e1839344116a67831846729a0c993
Aleksey Sokolov <alexis871aa@yandex.ru>
2024-11-25 23:20:29 +0300

    add in project password func reversewith pointer

--- main.go
+++ main.go
@@ -3,5 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello password!")
+	a := [4]int{1, 2, 3, 4}
+	reverse(&a) // меняет порядок элементов на обратный
+	fmt.Println(a)
+}
+
+func reverse(arr *[4]int) {
+	// другой вариант (якобы правильный)
+	for index, value := range *arr {
+		(*arr)[len(arr)-1-index] = value
+	}
+
+	// мой вариант
+	//for i := 0; i < len(*arr)/2; i++ {
+	//	tmp := (*arr)[i]
+	//	(*arr)[i] = (*arr)[len(*arr)-i-1]
+	//	(*arr)[len(*arr)-i-1] = tmp
+	//}
 }
