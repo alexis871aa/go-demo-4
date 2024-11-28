53ea8b5246fef7a734d2829b14f62023306996fa
Aleksey Sokolov <alexis871aa@yandex.ru>
2024-11-28 17:26:08 +0300

    Зафиналили задачку со структурами, отлично

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"math/rand/v2"
+	"net/url"
+	"time"
+)
 
 type account struct {
 	login    string
@@ -8,32 +14,84 @@ type account struct {
 	url      string
 }
 
+type accountWithTimeStamp struct {
+	createdAt time.Time
+	updatedAt time.Time
+	account
+}
+
+func (acc *accountWithTimeStamp) outputPassword() {
+	fmt.Println(acc.login, acc.password, acc.url)
+}
+
+func (acc *accountWithTimeStamp) generatePassword(n int) {
+	res := make([]rune, n)
+	for i := range res {
+		res[i] = letterRunes[rand.IntN(len(letterRunes))]
+	}
+	acc.password = string(res)
+}
+
+func newAccountWithTimeStamp(login, password, urlString string) (*accountWithTimeStamp, error) {
+	if login == "" {
+		return nil, errors.New("INVALID LOGIN")
+	}
+	_, err := url.ParseRequestURI(urlString)
+	if err != nil {
+		return nil, errors.New("INVALID URL")
+	}
+	newAcc := &accountWithTimeStamp{
+		createdAt: time.Now(),
+		updatedAt: time.Now(),
+		account: account{
+			login:    login,
+			password: password,
+			url:      urlString,
+		},
+	}
+	if password == "" {
+		newAcc.generatePassword(12)
+	}
+	return newAcc, nil
+}
+
+//func newAccount(login, password, urlString string) (*account, error) {
+//	if login == "" {
+//		return nil, errors.New("INVALID LOGIN")
+//	}
+//	_, err := url.ParseRequestURI(urlString)
+//	if err != nil {
+//		return nil, errors.New("INVALID URL")
+//	}
+//	newAcc := &account{
+//		login:    login,
+//		password: password,
+//		url:      urlString,
+//	}
+//	if password == "" {
+//		newAcc.generatePassword(12)
+//	}
+//	return newAcc, nil
+//}
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-*!")
+
 func main() {
-	str := []rune("Привет!)")
-	for _, ch := range string(str) {
-		fmt.Println(ch, string(ch))
+	login := promptData("Введите логин: ")
+	password := promptData("Введите пароль: ")
+	url := promptData("Введите URL: ")
+	myAccount, err := newAccountWithTimeStamp(login, password, url)
+	if err != nil {
+		fmt.Println("Неверный формат URL или Логин")
+		return
 	}
-	login := promptData("Введите логин")
-	password := promptData("Введите пароль")
-	url := promptData("Введите URL")
-
-	myAccount := account{
-		password: password,
-		url:      url,
-		login:    login,
-	}
-
-	outputPassword(&myAccount)
+	myAccount.outputPassword()
+	fmt.Println(myAccount)
 }
 
 func promptData(prompt string) string {
-	fmt.Println(prompt + ": ")
+	fmt.Print(prompt + ": ")
 	var res string
-	fmt.Scan(&res)
+	fmt.Scanln(&res)
 	return res
-}
-
-func outputPassword(acc *account) {
-	fmt.Println(acc)
-	fmt.Println(acc.login, acc.password, acc.url)
 }
